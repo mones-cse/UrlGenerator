42bd44d24c9d4f10206558665d88097811bcf8bd
monesul haque <mones0008@gmail.com>
2015-10-31 17:50:11 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-var myApp = angular.module('myApp', []);
+//create an an object that hold angular module
+var myApp = angular.module('myApp',[]);
 
 myApp.controller('mainController',['$scope',function($scope){
-		console.log($scope);
-		$scope.name="mones";
+	console.log($scope);
+	$scope.user;
+	$scope.links={};
+	
+	$scope.getLink= function(user){
+	angular.copy(user,$scope.links);
+		$scope.links.slash="/";
+		$scope.links.http="http://";
+		$scope.links.git=".github.io/";
+	}
+	
 }]);
